7a232922e628d27c2801f0d9f52098569432489a
JessicaLDaley <jessicadaleypaints@yahoo.com>
2021-09-01 19:59:13 -0400

    first commit

---
+++ assets/javascript/weather.js
@@ -0,0 +1,10 @@
+
+// API KEY
+// 95ce02ab9c38e7546c35a13777f50cf6
+
+var getWeather = function() {
+   var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,minutely&appid={95ce02ab9c38e7546c35a13777f50cf6}"
+}
+  
+  
+
