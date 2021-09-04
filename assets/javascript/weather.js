fc6cf7659c631b2a511f9aaee4980b92273d3f55
JessicaLDaley <jessicadaleypaints@yahoo.com>
2021-09-04 18:37:10 -0400

    Print data to page

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -1,7 +1,6 @@
 var button = document.querySelector(".btn")
 var inputValue = document.querySelector(".inputValue")
-var name = document.querySelector(".name");
-var desc = document.querySelector(".desc");
+var city = document.querySelector(".city");
 var temp = document.querySelector(".temp");
 var wind = document.querySelector(".wind");
 var humidity = document.querySelector(".humidity");
@@ -12,14 +11,38 @@ var uvIndex = document.querySelector(".uv-index");
 button.addEventListener("click",function(){
   fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=95ce02ab9c38e7546c35a13777f50cf6')
   .then(response => response.json())
-  .then(data => console.log(data))
+  .then(data => {
 
-  .catch(err => alert("Please enter a valid city!"))
+ 
+var cityValue = data["name"];
+var tempValue = data['main']['temp'];
+tempValue = ((tempValue-273.15)*1.8)+32;
+var humidityValue = data["main"]['humidity'];
+
+var windValue = data["wind"]["speed"];
+
+console.log(data);
+
+
+city.innerHTML = cityValue;
+temp.innerHTML = "Temp: " + tempValue;
+humidity.innerHTML = "Humidty: " + humidityValue;
+wind.innerHTML = "Wind: " + windValue;
+
+
+
+
+})
+.catch(err => alert("Please enter a valid city!"))
+
 })
 
-function weatherData(data){
-  
-}
+
+
+
+
+
+
 
 
 
