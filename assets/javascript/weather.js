ea27972efbcb1cf1f40d7d360867993183aa40f8
JessicaLDaley <jessicadaleypaints@yahoo.com>
2021-09-04 14:39:53 -0400

    successful api fetch

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -1,10 +1,41 @@
+var button = document.querySelector(".btn")
+var inputValue = document.querySelector(".inputValue")
+var name = document.querySelector(".name");
+var desc = document.querySelector(".desc");
+var temp = document.querySelector(".temp");
+var wind = document.querySelector(".wind");
+var humidity = document.querySelector(".humidity");
+var uvIndex = document.querySelector(".uv-index");
 
-// API KEY
-// 95ce02ab9c38e7546c35a13777f50cf6
 
-var getWeather = function() {
-   var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,minutely&appid={95ce02ab9c38e7546c35a13777f50cf6}"
+
+button.addEventListener("click",function(){
+  fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=95ce02ab9c38e7546c35a13777f50cf6')
+  .then(response => response.json())
+  .then(data => console.log(data))
+
+  .catch(err => alert("Please enter a valid city!"))
+})
+
+function weatherData(data){
+  
 }
-  
-  
+
+
+
+
+
+
+
+
+
+
+    
+   
+
+   
+   
+
+           
+
 
