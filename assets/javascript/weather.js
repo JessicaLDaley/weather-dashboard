26240bdfb865e68b36818f30f6d64f4b0aab9f8d
JessicaLDaley <jessicadaleypaints@yahoo.com>
2021-09-05 22:02:04 -0400

    added weather icons

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -45,15 +45,59 @@ button.addEventListener("click", function () {
       let j = 1;
       console.log(data);
       for (let i = 0; i < data.list.length; i++) {
-        //console.log(data.list[i].dt_txt.substring(10,13))
-        if (data.list[i].dt_txt.substring(10, 13) == 12) {
+       if (data.list[i].dt_txt.substring(10, 13) == 12) {
           console.log("found element " + data.list[i].dt_txt);
-          var cardTitle = document.querySelector("h5.card-title" + j); //document.getElementsByClassName("card-title"+j);
-          var cardText = document.querySelector("p.card-text" + j);//document.getElementsByClassName("card-text"+j);
+          var cardTitle = document.querySelector("h5.card-title" + j); 
+          var cardText = document.querySelector("p.card-text" + j);
           var tempValue = data.list[i].main.temp;
           tempValue = Math.round(((tempValue - 273.15) * 1.8) + 32);
           cardTitle.innerHTML = data.list[i].dt_txt.substring(0, 11);
-          cardText.innerHTML = "Temp: " + tempValue + "<br/> Wind: " + data.list[i].wind.speed + "<br/> Humidity: " + data.list[i].main.humidity;
+
+
+          var picture_url;
+          var nomatch = false;
+          if (data.list[i].weather[0].description == "clear sky") {
+            picture_url = "http://openweathermap.org/img/wn/01d@2x.png"
+
+          } else if (data.list[i].weather[0].description == "few clouds") {
+            picture_url = "http://openweathermap.org/img/wn/02d@2x.png"
+
+          } else if (data.list[i].weather[0].description == "scattered clouds") {
+            picture_url = "http://openweathermap.org/img/wn/03d@2x.png"
+
+          } else if (data.list[i].weather[0].description == "broken clouds") {
+            picture_url = "http://openweathermap.org/img/wn/04d@2x.png"
+
+          } else if (data.list[i].weather[0].description == "shower rain") {
+            picture_url = "http://openweathermap.org/img/wn/05d@2x.png"
+
+          } else if (data.list[i].weather[0].description == "rain") {
+            picture_url = "http://openweathermap.org/img/wn/06d@2x.png"
+
+          } else if (data.list[i].weather[0].description == "thunderstorm") {
+            picture_url = "http://openweathermap.org/img/wn/07d@2x.png"
+
+          } else if (data.list[i].weather[0].description == "snow") {
+            picture_url = "http://openweathermap.org/img/wn/08d@2x.png"
+
+
+          } else if (data.list[i].weather[0].description == "mist") {
+            picture_url = "http://openweathermap.org/img/wn/09d@2x.png"
+
+          } else { nomatch = true; }
+
+          if (!nomatch) {
+            cardText.innerHTML = "<img src=" + picture_url + "><br/> Temp: " + tempValue + "<br/> Wind: " + data.list[i].wind.speed + "<br/> Humidity: " + data.list[i].main.humidity;
+          } else {
+            cardText.innerHTML = "Temp: " + tempValue + "<br/> Wind: " + data.list[i].wind.speed + "<br/> Humidity: " + data.list[i].main.humidity;
+          }
+
+
+
+
+
+
+
           j = j + 1;
 
 
@@ -85,18 +129,17 @@ function updateSearchHistory(city) {
 
 
 
-    // Set variable equal to contents of window localstorage
+   
     var myStorage = window.localStorage;
 
     for (let i = 0; i < localStorage.length; i++) {
-      // iterate through contents of local storage. create one button for each city
-      var searchHistoryBtn = document.getElementsByClassName("historyBtns");
+     var searchHistoryBtn = document.getElementsByClassName("historyBtns");
       searchHistoryBtn[i].innerHTML = myStorage.key(i);
 
     }
   }
 
-  
+
 }
 
 
@@ -127,21 +170,63 @@ function updateSearch() {
       wind.innerHTML = "Wind: " + windValue + " mph";
     })
 
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + newSearch + "&appid=95ce02ab9c38e7546c35a13777f50cf6")
+  fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + newSearch + "&appid=95ce02ab9c38e7546c35a13777f50cf6")
     .then(response => response.json())
     .then(data => {
       let j = 1;
       console.log(data);
       for (let i = 0; i < data.list.length; i++) {
-        //console.log(data.list[i].dt_txt.substring(10,13))
         if (data.list[i].dt_txt.substring(10, 13) == 12) {
           console.log("found element " + data.list[i].dt_txt);
-          var cardTitle = document.querySelector("h5.card-title" + j); //document.getElementsByClassName("card-title"+j);
-          var cardText = document.querySelector("p.card-text" + j);//document.getElementsByClassName("card-text"+j);
+          var cardTitle = document.querySelector("h5.card-title" + j); 
+          var cardText = document.querySelector("p.card-text" + j);
           var tempValue = data.list[i].main.temp;
           tempValue = Math.round(((tempValue - 273.15) * 1.8) + 32);
           cardTitle.innerHTML = data.list[i].dt_txt.substring(0, 11);
-          cardText.innerHTML = "Temp: " + tempValue + "&#xb0;" + "<br/> Wind: " + data.list[i].wind.speed + "<br/> Humidity: " + data.list[i].main.humidity;
+          var picture_url;
+          var nomatch = false;
+          if (data.list[i].weather[0].description == "clear sky") {
+            picture_url = "http://openweathermap.org/img/wn/01d@2x.png"
+
+          } else if (data.list[i].weather[0].description == "few clouds") {
+            picture_url = "http://openweathermap.org/img/wn/02d@2x.png"
+
+          } else if (data.list[i].weather[0].description == "scattered clouds") {
+            picture_url = "http://openweathermap.org/img/wn/03d@2x.png"
+
+          } else if (data.list[i].weather[0].description == "broken clouds") {
+            picture_url = "http://openweathermap.org/img/wn/04d@2x.png"
+
+          } else if (data.list[i].weather[0].description == "shower rain") {
+            picture_url = "http://openweathermap.org/img/wn/05d@2x.png"
+
+          } else if (data.list[i].weather[0].description == "rain") {
+            picture_url = "http://openweathermap.org/img/wn/06d@2x.png"
+
+          } else if (data.list[i].weather[0].description == "thunderstorm") {
+            picture_url = "http://openweathermap.org/img/wn/07d@2x.png"
+
+          } else if (data.list[i].weather[0].description == "snow") {
+            picture_url = "http://openweathermap.org/img/wn/08d@2x.png"
+
+
+          } else if (data.list[i].weather[0].description == "mist") {
+            picture_url = "http://openweathermap.org/img/wn/09d@2x.png"
+
+          } else { nomatch = true; }
+
+          if (!nomatch) {
+            cardText.innerHTML = "<img src=" + picture_url + "><br/> Temp: " + tempValue + "<br/> Wind: " + data.list[i].wind.speed + "<br/> Humidity: " + data.list[i].main.humidity;
+          } else {
+            cardText.innerHTML = "Temp: " + tempValue + "<br/> Wind: " + data.list[i].wind.speed + "<br/> Humidity: " + data.list[i].main.humidity;
+          }
+
+
+
+
+
+
+
           j = j + 1;
 
 
